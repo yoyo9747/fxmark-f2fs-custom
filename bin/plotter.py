d90b66685bacba9fe4bf3a0395b1e68873b39bb6
yoyo9747 <josephro9747@gmail.com>
2024-12-13 15:59:45 +0000

    asdf

--- bin/plotter.py
+++ bin/plotter.py
@@ -142,7 +142,7 @@ class Plotter(object):
 
         fs = fs_list[0]
         # print("plot [0:][0:] \'%s\' using 1:2 title \'%s\' %s"
-        print("plot [0:64][0:0.5] \'%s\' using 1:2 title \'%s\' %s"
+        print("plot [0:70][0:0.4] \'%s\' using 1:2 title \'%s\' %s"
               % (_get_data_file(fs), fs, _get_sc_style(fs)),
               end="", file=self.out)
         for fs in fs_list[1:]:
